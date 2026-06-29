d3fe05808b7824b54fe55f5467ded4b3f8df4efc
buuuchuketa <buuuchuketa@gmail.com>
2026-06-29 17:23:54 +0900

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Pet Card Generator",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
